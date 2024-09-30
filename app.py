ee2764036643f25f790fa644e39f354e540e450d
w3cj <cj@null.computer>
2024-09-29 23:35:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -121,4 +121,4 @@ def run_manual_kmeans():
         return jsonify({'error': str(e)}), 500
     
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+    app.run(debug=True, host='0.0.0.0', port=3000)
